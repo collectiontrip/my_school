d501b17b688c06eb8b693001a9e11c2b2cde32d2
collectiontrip <rdochania92@gmail.com>
2026-02-12 13:08:08 +0530

    convert http in https for frontend and access camera and mic for media

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import fs from 'fs';
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '192.168.137.1',   // ✅ hotspot wali IP
+    host: '192.168.137.1', // LAN IP
     port: 5173,
-    strictPort: true
+    strictPort: true,
+    https: {
+      key: fs.readFileSync('./192.168.137.1-key.pem'),
+      cert: fs.readFileSync('./192.168.137.1.pem'),
+    }
   }
-})
+});
